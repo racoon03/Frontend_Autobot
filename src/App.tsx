0bf65a1cd10fd3d4f30cf3ebf58efeea7f1d82f4
HeatBlastzz <finalfanta3@gmail.com>
2025-05-28 09:07:37 +0700

    modify information and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,6 @@
-//import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-
-
-import TradingBotHeader from './layouts/Header';
-import TradingBotFooter from './layouts/Footer';
-//import NotFoundPage from './layouts/404Page';
 import './App.css'
-//import ServiceRates from './pages/Services_Rates';
-import ExtensionPage from './pages/Download_Extension';
-import DefaultLayout from './layouts/DefaultLayout';
-import LogHistory from './pages/LogHistory';
-import HomePage from './pages/Home_page';
-import TradingBotInfo from './pages/Information';
+import HomePage from './pages/Home';
+
 
 
 
@@ -23,8 +11,6 @@ function App() {
         <HomePage/>
     </>
 
-    // <NotFoundPage />
-    //<ServiceRates/>
   )
 }
 
